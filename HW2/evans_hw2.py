d328d733e4c1ff21028b0b9e56562b65a5944812
Nathaniel Evans <nathaniel.evansj@gmail.com>
2018-10-12 10:03:10 -0700

    solved the node order problem by sorting by distance from origin, not sure on the time complexity... but sleek solution programatically

--- HW2/evans_hw2.py
+++ HW2/evans_hw2.py
@@ -15,7 +15,7 @@ def make_mat(shape):
     # even indices represent edges (starting at 0), odd represent nodes 
     # dim has to be double seq lens then
     
-    return np.zeros( (shape[0]*2 + 3, shape[1]*2 + 3) )
+    return np.zeros( (shape[0]*2 + 1, shape[1]*2 + 1) )
 
 
 
@@ -51,6 +51,22 @@ def get_score(i, j, seq1, seq2):
     else: 
         return 0 # neither are HP
 
+'''
+def ordered_pop(i, j): 
+    global board
+    
+    for 
+    # fill yourself out then send on: 
+            # row first, then col, then next diag
+    from_above = board[i][j-2] + board[i][j-1] 
+    from_left = board[i-2][j] + board[i-1][j]
+    from_diag = board[i-2][j-2] + board[i-1][j-1]
+
+    board[i][j] = np.max([from_above, from_left, from_diag]) 
+'''
+
+def dist(tup): 
+    return ( (tup[0]**2 + tup[1]**2)**(0.5) ) 
 
 if __name__ == "__main__" :
     board = make_mat(SHAPE)
@@ -62,8 +78,7 @@ if __name__ == "__main__" :
             if (i==0 and j==0):
                 board[i,j]=0
             # indel position 
-            if (i == board.shape[0]-2 or j == board.shape[1]-2):
-                board[i][j] = -100
+            
             elif ( (i%2==1 and j%2==0) or (i%2==0 and j%2==1) ):
                 board[i][j] = -2
             
@@ -72,19 +87,28 @@ if __name__ == "__main__" :
 
     #print(np.array2string(board, max_line_width=np.inf))             
     #print()
-        
-    for i in range(0, board.shape[0] -2 , 2): 
-        for j in range(0, board.shape[1] -2, 2):
-            
-            if(i < board.shape[0]-2 and j < board.shape[1]-2 and i%2==0 and j%2==0 ):
-                # NEED TO ACCOUNT FOR EDGES - ITS wrapping
-                #     square two above above   right above
-                from_above = board[i][j-2] + board[i][j-1] 
-                from_left = board[i-2][j] + board[i-1][j]
-                from_diag = board[i-2][j-2] + board[i-1][j-1]
-                
-                board[i][j] = np.max([from_above, from_left, from_diag])            
-        
+        # create node indices, then sort by distance from 0 to create proper ordering 
+    node_order = []
+    for i in range(0,board.shape[0],2): 
+        for j in range(0, board.shape[1], 2): 
+            node_order.append( (i,j) )
+    
+    # sort by distance from origin
+    node_ordered = sorted(node_order, key=dist)
+    # remove 0,0 
+    node_ordered.remove((0,0))
+    
+    for node in node_ordered: 
+        i,j = node
+        max_ls = []
+        if (j>1): 
+            max_ls.append( board[i][j-2] + board[i][j-1] )
+        if (i>1): 
+            max_ls.append( board[i-2][j] + board[i-1][j] ) 
+        if (j>1 and i>1): 
+            max_ls.append( board[i-2][j-2] + board[i-1][j-1] )
+        board[i][j] = np.max(max_ls) 
+     
     print(np.array2string(board, max_line_width=np.inf))
     
     # populate match/mismatch - diag transitions
