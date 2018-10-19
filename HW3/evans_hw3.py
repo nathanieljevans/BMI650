59ab930875bb8de3317325868d2b50f243b745ad
Nathaniel Evans <nathaniel.evansj@gmail.com>
2018-10-18 17:26:29 -0700

    code functionality finished. output verified with (arguable diff) cohort. Need to comment, produce output.

--- HW3/evans_hw3.py
+++ HW3/evans_hw3.py
@@ -18,6 +18,44 @@ sum of pairs  (50 pts)
  
 Please utilize a BLOSUM62 substitution matrix for (ii)
 
+if columns have a gap, then ignore it 
+
+
+
+#  Matrix made by matblas from blosum62.iij
+#  * column uses minimum score
+#  BLOSUM Clustered Scoring Matrix in 1/2 Bit Units
+#  Blocks Database = /data/blocks_5.0/blocks.dat
+#  Cluster Percentage: >= 62
+#  Entropy =   0.6979, Expected =  -0.5209
+   A  R  N  D  C  Q  E  G  H  I  L  K  M  F  P  S  T  W  Y  V  B  Z  X  *
+A  4 -1 -2 -2  0 -1 -1  0 -2 -1 -1 -1 -1 -2 -1  1  0 -3 -2  0 -2 -1  0 -4 
+R -1  5  0 -2 -3  1  0 -2  0 -3 -2  2 -1 -3 -2 -1 -1 -3 -2 -3 -1  0 -1 -4 
+N -2  0  6  1 -3  0  0  0  1 -3 -3  0 -2 -3 -2  1  0 -4 -2 -3  3  0 -1 -4 
+D -2 -2  1  6 -3  0  2 -1 -1 -3 -4 -1 -3 -3 -1  0 -1 -4 -3 -3  4  1 -1 -4 
+C  0 -3 -3 -3  9 -3 -4 -3 -3 -1 -1 -3 -1 -2 -3 -1 -1 -2 -2 -1 -3 -3 -2 -4 
+Q -1  1  0  0 -3  5  2 -2  0 -3 -2  1  0 -3 -1  0 -1 -2 -1 -2  0  3 -1 -4 
+E -1  0  0  2 -4  2  5 -2  0 -3 -3  1 -2 -3 -1  0 -1 -3 -2 -2  1  4 -1 -4 
+G  0 -2  0 -1 -3 -2 -2  6 -2 -4 -4 -2 -3 -3 -2  0 -2 -2 -3 -3 -1 -2 -1 -4 
+H -2  0  1 -1 -3  0  0 -2  8 -3 -3 -1 -2 -1 -2 -1 -2 -2  2 -3  0  0 -1 -4 
+I -1 -3 -3 -3 -1 -3 -3 -4 -3  4  2 -3  1  0 -3 -2 -1 -3 -1  3 -3 -3 -1 -4 
+L -1 -2 -3 -4 -1 -2 -3 -4 -3  2  4 -2  2  0 -3 -2 -1 -2 -1  1 -4 -3 -1 -4 
+K -1  2  0 -1 -3  1  1 -2 -1 -3 -2  5 -1 -3 -1  0 -1 -3 -2 -2  0  1 -1 -4 
+M -1 -1 -2 -3 -1  0 -2 -3 -2  1  2 -1  5  0 -2 -1 -1 -1 -1  1 -3 -1 -1 -4 
+F -2 -3 -3 -3 -2 -3 -3 -3 -1  0  0 -3  0  6 -4 -2 -2  1  3 -1 -3 -3 -1 -4 
+P -1 -2 -2 -1 -3 -1 -1 -2 -2 -3 -3 -1 -2 -4  7 -1 -1 -4 -3 -2 -2 -1 -2 -4 
+S  1 -1  1  0 -1  0  0  0 -1 -2 -2  0 -1 -2 -1  4  1 -3 -2 -2  0  0  0 -4 
+T  0 -1  0 -1 -1 -1 -1 -2 -2 -1 -1 -1 -1 -2 -1  1  5 -2 -2  0 -1 -1  0 -4 
+W -3 -3 -4 -4 -2 -2 -3 -2 -2 -3 -2 -3 -1  1 -4 -3 -2 11  2 -3 -4 -3 -2 -4 
+Y -2 -2 -2 -3 -2 -1 -2 -3  2 -1 -1 -2 -1  3 -3 -2 -2  2  7 -1 -3 -2 -1 -4 
+V  0 -3 -3 -3 -1 -2 -2 -3 -3  3  1 -2  1 -1 -2 -2  0 -3 -1  4 -3 -2 -1 -4 
+B -2 -1  3  4 -3  0  1 -1  0 -3 -4  0 -3 -3 -2  0 -1 -4 -3 -3  4  1 -1 -4 
+Z -1  0  0  1 -3  3  4 -2  0 -3 -3  1 -1 -3 -1  0 -1 -3 -2 -2  1  4 -1 -4 
+X  0 -1 -1 -1 -2 -1 -1 -1 -1 -1 -1 -1 -1 -1 -2  0  0 -2 -1 -1 -1 -1 -1 -4 
+* -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4 -4  1 
+
+
+
 """
 
 import re
@@ -37,21 +75,32 @@ class substitution_matrix:
                     pass
                 else: 
                     if(header): 
-                        self.header = re.sub('[ *\n]','', line) # remove spaces
+                        self.header = re.sub('[ \n]*','', line) # remove spaces
                         header = False 
                     else: 
                         temp_table.append( [int(scr) for scr in re.split(' *', line[1:].strip()) ])
-            
+                        
             self.table = np.array(temp_table)
-            print('header:', self.header)
-            print('header length:', len(self.header))
+            
+            with open('./data/substitution_matrix.txt', 'w') as f: 
+                np.set_printoptions(precision=3,threshold=np.nan)
+                f.write(np.array2string(self.table, max_line_width=np.inf))
             
             
     def __get_aa_index__(self, aa): 
         return self.header.find(aa)
+        
     
-    def get_score(self, aa1, aa2): 
-        return self.table[self.__get_aa_index__(aa1), self.__get_aa_index__(aa2)]
+    def get_score(self, aa1, aa2, score_indel=True): 
+        #print( (aa1, aa2) )
+        if ( not score_indel and (aa1=='*' or aa2=='*')) :
+            return 0
+            
+        else:
+            return self.table[self.__get_aa_index__(aa1), self.__get_aa_index__(aa2)]
+
+        
+    
     
 
 class sequence_alignment: 
@@ -61,19 +110,74 @@ class sequence_alignment:
         assert max([len(x) for x in seqs]) == min([len(x) for x in seqs]), "sequence lengths must be the same length to key starting index, add indels to offset lengths to proper alignment" 
               
         self.score_matrix = sub_mat
-        self.seqs = seqs
+        self.seqs = np.array( [[aa for aa in seq] for seq in seqs] )
         self.len = len(seqs[0])
         self.num_seqs = len(seqs)
         
         self.__generate_entropy_matrix__()
         
-        with open('./data/entropy_matrix.txt', 'w') as f: 
-            np.set_printoptions(precision=3,threshold=np.nan)
-            f.write(np.array2string(self.__ent_mat__, max_line_width=np.inf))
+#        with open('./data/entropy_matrix.txt', 'w') as f: 
+#            np.set_printoptions(precision=3,threshold=np.nan)
+#            f.write(np.array2string(self.__ent_mat__, max_line_width=np.inf))
+#        
+#        with open('./data/entropy_matrix2.txt', 'w') as f: 
+#            np.set_printoptions(precision=3,threshold=np.nan)
+#            f.write(np.array2string(self.__ent_mat__, max_line_width=np.inf))
         
-        self.__score_alignment__()
+    def score(self, method='entropy', include_gaps=True): 
+        assert method == 'entropy' or method == 'sum_of_pairs', "unrecognized scoring method" 
         
+        if (method=='entropy') : 
+            self.__score_entropy_matrix__(include_indels=include_gaps)
+            return self.entropy_sum 
         
+        else: # sum of pairs 
+            return self.__score_sum_of_pair__(score_indel=include_gaps)
+        
+    def __pair_sum__(self, seqs_col, score_indel): 
+        score = 0
+        if len(seqs_col) > 1 :
+            aa1 = seqs_col[0]
+            for i,aa2 in enumerate(seqs_col[1:]): 
+                score += self.score_matrix.get_score(aa1,aa2,score_indel) + self.__pair_sum__(seqs_col[i+1:], score_indel)
+            return score
+        else: 
+            return 0
+                
+    
+    # this also includes cols with indels if there are non indel pairs to compare 
+    def __score_sum_of_pair__(self, score_indel): 
+        
+        score = 0
+        for i in range( self.len ): 
+            col = self.seqs[:,i]
+            score += self.__pair_sum__(col, score_indel)
+            
+        return score
+    
+    
+                # make sure to talk about including the indel with one gap in scoring (even with include_indel == false). Entropy created by indel is NOT included, but entropy created from the two other mismatches aas ARE included. 
+    def __score_entropy_matrix__(self, include_indels=True): 
+        
+        ent_vec = np.vectorize(self.__entropy__)
+        
+        if(not include_indels): 
+            # remove indel col (last )
+            self.__ent_mat__ = np.delete(self.__ent_mat__, -1, 1)
+            # normalize each row 
+            #x = np.array([LA.norm(v,ord=1) for v in X]
+            norm = np.transpose( np.tile((np.linalg.norm(self.__ent_mat__, axis=1, ord=1)) , (23,1) ) )
+            
+            self.__ent_mat__ = self.__ent_mat__ / norm # normalize rows of the indel-less entropy matrix
+        
+        self.entropy_sum = np.sum( ent_vec(self.__ent_mat__) ) 
+
+        
+    def __entropy__(self, pi): 
+        if (pi > 0 and pi < 1):
+            return -pi*np.log2(pi)
+        else:
+            return 0 
         
         
     def __generate_entropy_matrix__(self): 
@@ -87,23 +191,14 @@ class sequence_alignment:
         :
         *  0   0
         
-        TODO: I think this is printed cols as rows, arbitrary, just note. Also, HEADER IS WRONG NUM OF CHARACTERS, len = 23, should be 22 
         '''
         self.__ent_mat__ = np.zeros([self.len, len(self.score_matrix.header)])
         
         for i in range(self.len): 
             for j in range(self.num_seqs): 
-                aa = self.seqs[j][i]
-                self.__ent_mat__[i][self.score_matrix.__get_aa_index__(aa)] += 1/self.num_seqs
+                aa = self.seqs[j,i]
+                self.__ent_mat__[i, self.score_matrix.__get_aa_index__(aa)] += 1/self.num_seqs
                 
-        print(self.num_seqs)
-                
-                
-        
-    def __score_alignment__(self): 
-        pass
-    
-    
                      
 
 if __name__ == '__main__' : 
@@ -115,15 +210,24 @@ if __name__ == '__main__' :
     with open('./data/hw3.txt') as f: 
         for line in f.readlines(): 
             if (line.strip()):
-                seqs.append(line.strip())
-                print(len(line.strip()))
-                print(line.strip())
+                test = re.sub('-', '*', line.strip())
+                seqs.append(re.sub('-', '*', line.strip()))
+                
 
         
     seq_align = sequence_alignment(smat, seqs) 
                 
-
-        
+    es_g = seq_align.score(method='entropy', include_gaps=True)
+    print('entropy score with gaps included:', es_g)
+    
+    es_ng = seq_align.score(method='entropy', include_gaps=False) # this actually alters the object, future calls will not be able to include gaps without reinitializing the object. 
+    print('entropy score, gaps not included:',es_ng)
+    
+    sop = seq_align.score(method='sum_of_pairs', include_gaps=True) 
+    print('sum of pairs gaps included:',sop )
+    
+    sop_ng = seq_align.score(method='sum_of_pairs', include_gaps=False) 
+    print('sum of pairs gaps not included:',sop_ng )
         
         
         
