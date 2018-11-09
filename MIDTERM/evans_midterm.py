ba9199bf61286b8e4bb3798e241a96767eaa4dad
Nathaniel Evans <nathaniel.evansj@gmail.com>
2018-11-09 10:29:49 -0800

    added midterm .docx

--- MIDTERM/evans_midterm.py
+++ MIDTERM/evans_midterm.py
@@ -10,12 +10,14 @@ Created on Tue Nov  6 17:08:23 2018
 """
 import numpy as np 
 import re
+import networkx as nx 
+from matplotlib import pyplot as plt
 
 S1 = 'GWWPDT' 
 S2 = 'WRRKHY'
 
     
-    
+ #### PROBLEM 1 ####   
 #----------------------------------------------------------
 
 def generate_BW_transform(seq): 
@@ -51,7 +53,7 @@ def  build_row(row_index, FC, LC):
 		
 def find_next(cur, b_rank, FC, LC): 
     '''
-    Slightly modified from HW4 - converted to generator 
+    Slightly modified from HW4
     finds the next character in the given suffix (eg row), as defined by cur and b_rank. 
     
     inputs
@@ -100,12 +102,74 @@ def search_for_shortest_unique_substring_length(S) :
             rows[j] += next(row_generators[j])
     
     return (len(S)) # no shortest substring found 
+#-------------------------------------------------------
+    #### PROBLEM 6 #### 
+#-------------------------------------------------------------------------------
+    
+def is_circular_rotation(s1,s2): 
+    for i in range(len(s1)): 
+        if (s1[i:] + s1[:i] == s2): 
+            return True 
+        
+    return False 
+
+#-------------------------------------------------------------------------------
+#Runs in O(n) where n is the length of S1/S2 
+#-------------------------------------------------------------------------------
+   #### PROBLEM 7 ####
+'''
+def rec_search(node, tree): 
+	count = 0 
+	If (node.has_no_children()): 
+		return 1 
+	
+	For child in node.get_children(): 
+		count += rec_search (child, tree) 
+	
+	return count 
+-------------------------------------------------------------------------------
+num_distinct_substrings = rec_search(root, tree) 
+'''	
+
+#-------------------------------------------------------------------------------
+
+
+
+#-------------------------------------------------------
 
 if __name__ == '__main__' : 
 
     shortest_unique_str_len = search_for_shortest_unique_substring_length('ATGATGATG$')
     print('answer:', shortest_unique_str_len)   
     
+    s1 = 'TTGATC' 
+    s2 = 'ATCTTG'
+    
+    print('circ rot?', is_circular_rotation(s1,s2))
+    
+    G = nx.Graph() 
+    G.add_node(0)
+    G.add_node(1) 
+    G.add_edge(0,1, label='NA')
+    G.add_node(2)
+    G.add_edge(1,2, label='$')
+    G.add_node(3)
+    G.add_edge(1,3, label='$')
+    #nx.draw(G, with_labels=True, edge)
+
+    
+    nx.draw_networkx(G)
+# Draw the node labels
+    nx.draw_networkx_labels(G)
+# Draw the edges
+    nx.draw_networkx_edges(G)
+# Draw the edge labels
+    nx.draw_networkx_edge_labels(G, edge_labels=nx.get_edge_attributes(G, 'label'))
+    # Remove the axis
+    plt.axis('off')
+    # Show the plot
+    plt.show()
+        
 #---------------------------------------------------------
         
         
